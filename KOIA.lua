473aa98748bfc1323cb337f12d69158086d47ffe
Sajjad31310 <100413761+Sajjad31310@users.noreply.github.com>
2022-05-16 19:58:15 +0300

    Update KOIA.lua

--- KOIA.lua
+++ KOIA.lua
@@ -6077,14 +6077,14 @@ local MsgId = msg.id/2097152/0.5
 local MSGID = string.gsub(MsgId,'.0','')
 https.request("https://api.telegram.org/bot"..Token..'/sendphoto?chat_id=' .. msg.chat_id .. '&photo=https://t.me/MoviesDavid/'..Abs..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..MsgId.."&parse_mode=markdown") 
 end
-if text == "انمي" or text == "انميي" or text == "انميي" then 
+if text == "انمي" or text == "انميي" or text == "انميي" then
 Abs = math.random(2,140); 
 local Text ='*✯︙تم اختيار انمي لك*'
 local MsgId = msg.id/2097152/0.5
 local MSGID = string.gsub(MsgId,'.0','')
 https.request("https://api.telegram.org/bot"..Token..'/sendphoto?chat_id=' .. msg.chat_id .. '&photo=https://t.me/AnimeDavid/'..Abs..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..MsgId.."&parse_mode=markdown") 
 end
-if text ==  "زواج" then
+if text ==  "زوجني" then
 return LuaTele.sendText(msg_chat_id,msg_id,'\n᥀︙ تم زواج : '..msg.Name_Controller,"md",true)
 end
 if text == "صوره" or text == "صورة" then
